bc373faa9bf8156a3cb73164d17de7fccaee52b2
Blacklands <Blacklands@gmx.net>
2025-01-22 17:03:26 +0100

    Improve Explorer session restoration process
    We now actually wait for each Explorer window to fully open before we
    continue, instead of having a hard-coded delay.
    
    We now also launch a separate process for each Explorer window. This
    does of course use more system memory, but it should improve robustness
    in case of one of the windows crashing. (It is also possible that this
    only works if the "start a new process for each window" option for
    Explorer is set in Windows, and that if it isn't, Windows consolidates
    the Explorer windows into fewer processes anyway. I tried testing it
    briefly, but couldn't see a difference. It is possible that this option
    is broken right now, anyway.)
    
    Furthermore, we now minimize the windows automatically after opening
    them. It is also possible to open them minimized right away, but this
    comes with its own issue (no taskbar previews shown until a window is
    brought to the foreground manually).
    
    This solves most of the issues we still had with this process,
    previously.

--- Restore-Session.ps1
+++ Restore-Session.ps1
@@ -1,11 +1,13 @@
 #requires -PSEdition Core
 
+using module ".\Set-WindowState.psm1"
+
 param (
     [Parameter(Mandatory=$true)][string]$backupsPath,
     [Parameter(Mandatory=$false)][string]$sessionFile
 )
 
-function WriteErrorAndQuit {
+function WriteError {
     
     param (
         [string]$error
@@ -15,10 +17,19 @@ function WriteErrorAndQuit {
     [Console]::ForegroundColor = 'red'
     [Console]::Error.WriteLine($error)
     [Console]::ResetColor()
+}
+
+function WriteErrorAndQuit {
+    
+    param (
+        [string]$error
+    )
+    
+    WriteError $error
     Exit 1
 }
 
-if ((Test-Path $backupsPath -PathType Container) -eq $false) {
+if ((Test-Path -LiteralPath $backupsPath -PathType Container) -eq $false) {
         WriteErrorAndQuit("The path for the backup files provided, `"$backupsPath`", does not exist, or is not a path to a folder. Quitting.")
 }
 
@@ -50,7 +61,7 @@ else {
 
 $restoreFilePath = Join-Path -Path $backupsPath -ChildPath $restoreFile
 
-if ((Test-Path $restoreFilePath -PathType Leaf) -eq $false) {
+if ((Test-Path -LiteralPath $restoreFilePath -PathType Leaf) -eq $false) {
         WriteErrorAndQuit("The given file name, `"$restoreFile`", does not seem to point to an actual file. Quitting.")       
 }
 
@@ -68,11 +79,43 @@ Write-Host "Restoring Windows Explorer session consisting of the following $($fo
 $folders | ForEach-Object { "- `"$_`"" } | Write-Host -Separator "\n"
 
 foreach ($folder in $folders) {
+    
+    # If we try to open an invalid folder path with Explorer, it won't skip it, it will open a generic folder (apparently the
+    # "Documents" folder of the user) instead, which we don't want. So we need to manually check and skip those.
+    if ((Test-Path -LiteralPath $folder) -eq $false) {
+        WriteError "The folder `"$folder`" does not seem to exist, or is not accessible. Skipping it."
+        continue
+    }
+    
     Write-Host "Opening folder `"$folder`"..."
-    # Need "-LiteralPath" here or "functional characters" like "[" and "]" break "Invoke-Item".
-    Invoke-Item -LiteralPath $folder
-    # If we open folders too quickly at once, Windows Explorer can apparently crash. So let's give it more time.
-    Start-Sleep -Milliseconds 100
+    # We're using "-Wait", also because things break if trying to launch Windows Explorer processess too fast while also using the
+    # "Miminized" window style - the Explorer windows don't open correctly and/or are super tiny and render wrong.
+    # "-Wait" is pretty slow. This is probably because we are actually starting an entirely separate process
+    # for each window. This is actually good because it prevents all windows from crashing and closing if there is an issue with a
+    # single one. This is supposed to be a toggleable option in Explorer anyway, but the option has been broken and does not work for
+    # (I think) years at this point.
+    # This likely uses a lot more system memory. But who cares? We have enough.
+    $process = Start-Process -PassThru -Wait -WindowStyle Normal -FilePath explorer.exe -ArgumentList "$folder"
+    # We're doing this instead of using the "Minimized" window style option above because if the windows are starting off minimized,
+    # Windows does not render a taskbar preview for any of them (they will just be blank) until they have been brought to the foreground
+    # once.
+    # Opening them in the foreground for a moment and then minimizing them makes that work.
+    # The disadvantage of this is that the "ALT + TAB" menu will get messed up, with all the windows at the top (since they were just in
+    # focus), which does not happen if they are opened in a minimized state.
+    # I have not been able to find a way to "refresh" the minimized windows without bringing them to the foreground, so I don't think
+    # a "best of both worlds" solution exists - at least not without me diving deeper into the Windows API and seeing if there is some
+    # low-level way to do this.
+    # 
+    # Also, we need to "generically" get all processes with the name "explorer" here, which is also not ideal. For some reason, the
+    # process that "Start-Process" returns has the wrong ID (one that does not exist) and does not actually point to the Explorer
+    # process that gets started - we have no idea what the ID of that process is, and we can't even find it by name, either.
+    # Yes, this means that all Explorer windows will get minimized when this script is run, even the ones that were already open before.
+    # I don't think we can do anything about that if we're using this method.
+    Get-Process "explorer" | Set-WindowState -State MINIMIZE
+    # Alternative way of giving Explorer more time between launches. Does NOT seem to consistently work properly with the "Minimized"
+    # window style option, things can still break. Probably okay with the "Normal" option, but still iffy. Explorer really needs some
+    # time between launches, so using the "-Wait" option makes more sense I think.
+    # Start-Sleep -Milliseconds 200
 }
 
 Write-Host "Successfully restored Explorer session."
